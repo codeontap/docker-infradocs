c005948f7d9287ac9db778737175ac3e037da371
Ilya Nemykin <ilya@retailenclicks.nl>
2018-06-07 12:59:12 +0300

    Update naviagation menu generation #3

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,42 +1,11 @@
-function openSideBarDeployments(evt, currentLevel, nextLevel) {
-
-    var i;
-    var x = document.getElementsByClassName("nav-"+currentLevel);
-    for (i = 0; i < x.length; i++) {
-
-        if (x[i].className.indexOf("w3-hide") == -1 ) {
-            x[i].className += " w3-hide";
+function changeOpenMenus() {
+    var treeMenuElements = document.querySelectorAll("#tree li");
+    for (i in treeMenuElements)
+    {
+        if (treeMenuElements[i].classList && treeMenuElements[i].classList.contains('collapsible-list-open')){
+            treeMenuElements[i].className = 'collapsibleListOpen';
+            treeMenuElements[i].firstElementChild.style.display = 'block';
         }
+
     }
-    
-    var levelBar = document.getElementById("level-"+currentLevel)
-    levelBar.className += " w3-show";
-    var currentText = document.getElementById(evt.currentTarget.id).innerText;
-    document.getElementById("level-text-"+currentLevel).innerText = currentText;
-
-    document.getElementById("level-current").innerText = nextLevel;
-
-    var deploymentNavs = document.getElementsByClassName("deployment-nav");
-    
-    for ( i = 0; i < deploymentNavs.length; i++ ) {
-        
-        if (deploymentNavs[i].id.indexOf(evt.currentTarget.id) >= 0 && deploymentNavs[i].classList.contains("deployment-"+nextLevel) ) {
-            if (deploymentNavs[i].className.indexOf("w3-hide") >= 0 ) {
-                deploymentNavs[i].className = deploymentNavs[i].className.replace(" w3-hide", "");
-            }
-            if (deploymentNavs[i].className.indexOf("w3-show") == -1 ) {
-                deploymentNavs[i].className += " w3-show";
-            }
-            console.log(evt.currentTarget.className)
-        }
-        else {
-            if (deploymentNavs[i].className.indexOf("w3-show") >= 0 ) {
-                deploymentNavs[i].className = deploymentNavs[i].className.replace(" w3-show", "");
-            }
-            if (deploymentNavs[i].className.indexOf("w3-hide") == -1 ) {
-                deploymentNavs[i].className += " w3-hide";
-            }
-        }
-    }
-
 }
